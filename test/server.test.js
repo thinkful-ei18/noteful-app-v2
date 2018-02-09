82114d9cd467093ef0ee97e5ee072783692fee81
Chris Klanac <cklanac@users.noreply.github.com>
2018-02-09 13:37:31 -0600

    move hooks into describe block

--- test/server.test.js
+++ test/server.test.js
@@ -12,21 +12,6 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 chai.use(chaiSpies);
 
-before(function () {
-  // noop
-});
-
-beforeEach(function () {
-  return seedData();
-});
-
-afterEach(function () {
-  // noop
-});
-
-after(function () {
-  return knex.destroy(); // destroy the connection
-});
 
 describe('Reality Check', () => {
 
@@ -86,259 +71,279 @@ describe('Basic Express setup', () => {
   });
 });
 
-describe('GET /v2/notes', function () {
-
-  it('should return the default of 10 Notes ', function () {
-    let count;
-    return knex.count()
-      .from('notes')
-      .then(([result]) => {
-        count = Number(result.count);
-        return chai.request(app).get('/v2/notes');
-      })
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body).to.have.length(count);
-      });
+describe('Noteful API', function () {
+  before(function () {
+    // noop
   });
 
-  it('should return a list with the correct right fields', function () {
-    return chai.request(app)
-      .get('/v2/notes')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body).to.have.length(10);
-        res.body.forEach(function (item) {
-          expect(item).to.be.a('object');
-          expect(item).to.include.keys('id', 'title', 'content');
+  beforeEach(function () {
+    return seedData();
+  });
+
+  afterEach(function () {
+    // noop
+  });
+
+  after(function () {
+    return knex.destroy(); // destroy the connection
+  });
+
+
+
+  describe('GET /v2/notes', function () {
+
+    it('should return the default of 10 Notes ', function () {
+      let count;
+      return knex.count()
+        .from('notes')
+        .then(([result]) => {
+          count = Number(result.count);
+          return chai.request(app).get('/v2/notes');
+        })
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(count);
         });
-      });
+    });
+
+    it('should return a list with the correct right fields', function () {
+      return chai.request(app)
+        .get('/v2/notes')
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(10);
+          res.body.forEach(function (item) {
+            expect(item).to.be.a('object');
+            expect(item).to.include.keys('id', 'title', 'content');
+          });
+        });
+    });
+
+
+    it('should return correct search results for a searchTerm query', function () {
+      let res;
+      return chai.request(app).get('/v2/notes?searchTerm=gaga')
+        .then(function (_res) {
+          res = _res;
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(1);
+          expect(res.body[0]).to.be.an('object');
+          return knex.select().from('notes').where('title', 'like', '%gaga%');
+        })
+        .then(data => {
+          expect(res.body[0].id).to.equal(data[0].id);
+        });
+    });
+
+
+    it('should search by folder id', function () {
+      const dataPromise = knex.select()
+        .from('notes')
+        .where('folder_id', 103)
+        .orderBy('notes.id');
+
+      const apiPromise = chai.request(app)
+        .get('/v2/notes?folderId=103');
+
+      return Promise.all([dataPromise, apiPromise])
+        .then(function ([data, res]) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(2);
+          expect(res.body[0]).to.be.an('object');
+          expect(res.body[0].id).to.equal(data[0].id);
+        });
+    });
+
+
+    it('should return an empty array for an incorrect query', function () {
+      return chai.request(app)
+        .get('/v2/notes?searchTerm=Not%20a%20Valid%20Search')
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('array');
+          expect(res.body).to.have.length(0);
+        });
+    });
+
   });
 
-  
-  it('should return correct search results for a searchTerm query', function () {
-    let res;
-    return chai.request(app).get('/v2/notes?searchTerm=gaga')
-      .then(function (_res) {
-        res = _res;
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body).to.have.length(1);
-        expect(res.body[0]).to.be.an('object');
-        return knex.select().from('notes').where('title', 'like', '%gaga%');
-      })
-      .then(data => {
-        expect(res.body[0].id).to.equal(data[0].id);
-      });
+  describe('GET /v2/notes/:id', function () {
+
+    it('should return correct notes', function () {
+      return chai.request(app)
+        .get('/v2/notes/1000')
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys('id', 'title', 'content');
+          expect(res.body.id).to.equal(1000);
+          expect(res.body.title).to.equal('5 life lessons learned from cats');
+        });
+    });
+
+    it('should respond with a 404 for an invalid id', function () {
+      const spy = chai.spy();
+      return chai.request(app)
+        .get('/v2/notes/9999')
+        .then(spy)
+        .then(() => {
+          expect(spy).to.not.have.been.called();
+        })
+        .catch(err => {
+          expect(err.response).to.have.status(404);
+        });
+    });
+
   });
 
+  describe('POST /v2/notes', function () {
 
-  it('should search by folder id', function () {
-    const dataPromise = knex.select()
-      .from('notes')
-      .where('folder_id', 103)
-      .orderBy('notes.id');
+    it('should create and return a new item when provided valid data', function () {
+      const newItem = {
+        'title': 'The best article about cats ever!',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+        'tags': []
+      };
+      let body;
+      return chai.request(app)
+        .post('/v2/notes')
+        .send(newItem)
+        .then(function (res) {
+          body = res.body;
+          expect(res).to.have.status(201);
+          expect(res).to.have.header('location');
+          expect(res).to.be.json;
+          expect(body).to.be.a('object');
+          expect(body).to.include.keys('id', 'title', 'content');
+          return knex.select().from('notes').where('id', body.id);
+        })
+        .then(([data]) => {
+          expect(body.title).to.equal(data.title);
+          expect(body.content).to.equal(data.content);
+        });
+    });
 
-    const apiPromise = chai.request(app)
-      .get('/v2/notes?folderId=103');
+    it('should return an error when missing "title" field', function () {
+      const newItem = {
+        'foo': 'bar'
+      };
+      const spy = chai.spy();
+      return chai.request(app)
+        .post('/v2/notes')
+        .send(newItem)
+        .then(spy)
+        .then(() => {
+          expect(spy).to.not.have.been.called();
+        })
+        .catch((err) => {
+          const res = err.response;
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `title` in request body');
+        });
+    });
 
-    return Promise.all([dataPromise, apiPromise])
-      .then(function ([data, res]) {        
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body).to.have.length(2);
-        expect(res.body[0]).to.be.an('object');
-        expect(res.body[0].id).to.equal(data[0].id);
-      });  
   });
 
+  describe('PUT /v2/notes/:id', function () {
 
-  it('should return an empty array for an incorrect query', function () {
-    return chai.request(app)
-      .get('/v2/notes?searchTerm=Not%20a%20Valid%20Search')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body).to.have.length(0);
-      });
+    it('should update the note', function () {
+      const updateItem = {
+        'title': 'What about dogs?!',
+        'content': 'woof woof',
+        'tags': []
+      };
+      return chai.request(app)
+        .put('/v2/notes/1005')
+        .send(updateItem)
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.include.keys('id', 'title', 'content');
+
+          expect(res.body.id).to.equal(1005);
+          expect(res.body.title).to.equal(updateItem.title);
+          expect(res.body.content).to.equal(updateItem.content);
+        });
+    });
+
+    it('should respond with a 404 for an invalid id', function () {
+      const updateItem = {
+        'title': 'What about dogs?!',
+        'content': 'woof woof',
+        'tags': []
+      };
+      const spy = chai.spy();
+      return chai.request(app)
+        .put('/v2/notes/9999')
+        .send(updateItem)
+        .then(spy)
+        .then(() => {
+          expect(spy).to.not.have.been.called();
+        })
+        .catch(err => {
+          expect(err.response).to.have.status(404);
+        });
+    });
+
+    it('should return an error when missing "title" field', function () {
+      const updateItem = {
+        'foo': 'bar'
+      };
+      const spy = chai.spy();
+      return chai.request(app)
+        .put('/v2/notes/9999')
+        .send(updateItem)
+        .then(spy)
+        .then(() => {
+          expect(spy).to.not.have.been.called();
+        })
+        .catch(err => {
+          const res = err.response;
+          expect(res).to.have.status(400);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a('object');
+          expect(res.body.message).to.equal('Missing `title` in request body');
+        });
+    });
+
+  });
+
+  describe('DELETE  /v2/notes/:id', function () {
+
+    it('should delete an item by id', function () {
+      return chai.request(app)
+        .delete('/v2/notes/1005')
+        .then(function (res) {
+          expect(res).to.have.status(204);
+        });
+    });
+
+    it('should respond with a 404 for an invalid id', function () {
+      const spy = chai.spy();
+      return chai.request(app)
+        .delete('/v2/notes/9999')
+        .then(spy)
+        .then(() => {
+          expect(spy).to.not.have.been.called();
+        })
+        .catch(err => {
+          expect(err.response).to.have.status(404);
+        });
+    });
+
   });
 
 });
-
-describe('GET /v2/notes/:id', function () {
-
-  it('should return correct notes', function () {
-    return chai.request(app)
-      .get('/v2/notes/1000')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.an('object');
-        expect(res.body).to.include.keys('id', 'title', 'content');
-        expect(res.body.id).to.equal(1000);
-        expect(res.body.title).to.equal('5 life lessons learned from cats');
-      });
-  });
-
-  it('should respond with a 404 for an invalid id', function () {
-    const spy = chai.spy();
-    return chai.request(app)
-      .get('/v2/notes/9999')
-      .then(spy)
-      .then(() => {
-        expect(spy).to.not.have.been.called();
-      })
-      .catch(err => {
-        expect(err.response).to.have.status(404);
-      });
-  });
-
-});
-
-describe('POST /v2/notes', function () {
-
-  it('should create and return a new item when provided valid data', function () {
-    const newItem = {
-      'title': 'The best article about cats ever!',
-      'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
-      'tags': []
-    };
-    let body;
-    return chai.request(app)
-      .post('/v2/notes')
-      .send(newItem)
-      .then(function (res) {
-        body = res.body;
-        expect(res).to.have.status(201);
-        expect(res).to.have.header('location');
-        expect(res).to.be.json;
-        expect(body).to.be.a('object');
-        expect(body).to.include.keys('id', 'title', 'content');
-        return knex.select().from('notes').where('id', body.id);
-      })
-      .then(([data]) => {
-        expect(body.title).to.equal(data.title);
-        expect(body.content).to.equal(data.content);
-      });
-  });
-
-  it('should return an error when missing "title" field', function () {
-    const newItem = {
-      'foo': 'bar'
-    };
-    const spy = chai.spy();
-    return chai.request(app)
-      .post('/v2/notes')
-      .send(newItem)
-      .then(spy)
-      .then(() => {
-        expect(spy).to.not.have.been.called();
-      })
-      .catch((err) => {
-        const res = err.response;
-        expect(res).to.have.status(400);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body.message).to.equal('Missing `title` in request body');
-      });
-  });
-
-});
-
-describe('PUT /v2/notes/:id', function () {
-
-  it('should update the note', function () {
-    const updateItem = {
-      'title': 'What about dogs?!',
-      'content': 'woof woof',
-      'tags': []
-    };
-    return chai.request(app)
-      .put('/v2/notes/1005')
-      .send(updateItem)
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('id', 'title', 'content');
-
-        expect(res.body.id).to.equal(1005);
-        expect(res.body.title).to.equal(updateItem.title);
-        expect(res.body.content).to.equal(updateItem.content);
-      });
-  });
-
-  it('should respond with a 404 for an invalid id', function () {
-    const updateItem = {
-      'title': 'What about dogs?!',
-      'content': 'woof woof',
-      'tags': []
-    };
-    const spy = chai.spy();
-    return chai.request(app)
-      .put('/v2/notes/9999')
-      .send(updateItem)
-      .then(spy)
-      .then(() => {
-        expect(spy).to.not.have.been.called();
-      })
-      .catch(err => {
-        expect(err.response).to.have.status(404);
-      });
-  });
-
-  it('should return an error when missing "title" field', function () {
-    const updateItem = {
-      'foo': 'bar'
-    };
-    const spy = chai.spy();
-    return chai.request(app)
-      .put('/v2/notes/9999')
-      .send(updateItem)
-      .then(spy)
-      .then(() => {
-        expect(spy).to.not.have.been.called();
-      })
-      .catch(err => {
-        const res = err.response;
-        expect(res).to.have.status(400);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body.message).to.equal('Missing `title` in request body');
-      });
-  });
-
-});
-
-describe('DELETE  /v2/notes/:id', function () {
-
-  it('should delete an item by id', function () {
-    return chai.request(app)
-      .delete('/v2/notes/1005')
-      .then(function (res) {
-        expect(res).to.have.status(204);
-      });
-  });
-
-  it('should respond with a 404 for an invalid id', function () {
-    const spy = chai.spy();
-    return chai.request(app)
-      .delete('/v2/notes/9999')
-      .then(spy)
-      .then(() => {
-        expect(spy).to.not.have.been.called();
-      })
-      .catch(err => {
-        expect(err.response).to.have.status(404);
-      });
-  });
-
-});
-
